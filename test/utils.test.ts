7b9028fb2bde37a4d832559f6a8bb795d84e4d11
sh <475784041@qq.com>
2019-09-13 15:41:27 +0800

    feat: 添加测试用例

---
+++ test/utils.test.ts
@@ -0,0 +1,24 @@
+import { findIndex, findKey, differenceBy } from '../src/utils'
+
+describe('utils 方法测试', () => {
+  const arr = [{id: 1}, {id: 2}, {id: 3}, {id: 4}]
+  it('findIndex 方法', () => {
+    expect(findIndex(arr, (item) => item.id === 1)).toBe(0)
+    expect(findIndex(arr, (item) => item.id === 5)).toBe(-1)
+  })
+  it('findKey 方法', () => {
+    const map = {
+      1: {val:1},
+      2: {val:2},
+      3: {val:3},
+    }
+    expect(findKey(map, (item) => item.val === 1)).toBe('1')
+    expect(findKey(map, (item) => item.val === 5)).toBe(undefined)
+  })
+  it('differenceBy 方法', () => {
+    const arr2 = [{id:1}, {id:3}, {id:5}]
+    expect(differenceBy(arr, arr2, 'id')).toMatchObject([{id:2}, {id:4}])
+    expect(differenceBy(arr2, arr, 'id')).toMatchObject([{id:5}])
+    
+  })
+})
