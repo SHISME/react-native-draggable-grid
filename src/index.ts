914a4b30148c349c61afa2675086922b5cfc4c66
suhai <suhai@kuaishou.com>
2022-02-17 09:17:25 +0800

    fix ts error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { IDraggableGridProps, DraggableGrid } from './draggable-grid'
 
-export { DraggableGrid, IDraggableGridProps }
+export { DraggableGrid }
+
+export type { IDraggableGridProps }
 
 export default DraggableGrid
